40a22261037801f2f31c065d158f312afb87f5c6
Rafael Piloto <31188361+RafaelPiloto10@users.noreply.github.com>
2020-04-09 20:06:46 -0400

    initial commit

---
+++ Electromagnet.ino
@@ -0,0 +1,35 @@
+// Motor A connections
+int enA = 9;
+int in1 = 8;
+int in2 = 7;
+
+
+void setup() {
+	pinMode(enA, OUTPUT);
+	pinMode(in1, OUTPUT);
+	pinMode(in2, OUTPUT);
+	
+	// Turn off  - Initial state
+	digitalWrite(in1, LOW);
+	digitalWrite(in2, LOW);
+}
+
+void loop() {
+	magnetOn();
+}
+
+void magnetOn(){
+	analogWrite(enA, 255);
+
+	digitalWrite(in1, HIGH);
+	digitalWrite(in2, LOW);
+	
+	delay(2000);
+}
+
+void magnetOff(){
+	digitalWrite(in1, LOW);
+	digitalWrite(in2, LOW);
+	digitalWrite(in3, LOW);
+	digitalWrite(in4, LOW);
+}
