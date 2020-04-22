3163dd27e74d9f913071e38337a5850a8824f6c3
Rafael Piloto <31188361+RafaelPiloto10@users.noreply.github.com>
2020-04-21 21:03:03 -0400

    fixed true -> True

--- controller.py
+++ controller.py
@@ -9,7 +9,7 @@ OFF = '0'
 if __name__ == "__main__":
 	arduino = serial.Serial(PORT, BAUDRATE)
 	if arduino.is_open:
-		while true:
+		while True:
 			state = int(input("Enter the state of the magnet (0 - off, 1 - on) or 69 to turn it off: "))
 			if state == 69:
 				arduino.close()
